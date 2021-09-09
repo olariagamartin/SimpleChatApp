cd9d5b82f38c6dc9984d40306f658265b76bcfef
Olariaga Martin <olariagamartin@gmail.com>
2021-09-08 23:54:17 -0300

    Fetch all users and put into a list

--- app/src/main/java/com/themarto/mychatapp/ChatActivity.java
+++ app/src/main/java/com/themarto/mychatapp/ChatActivity.java
@@ -1,16 +1,89 @@
 package com.themarto.mychatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.auth.User;
 import com.google.firebase.udacity.mychatapp.R;
+import com.google.firebase.udacity.mychatapp.databinding.ActivityChatBinding;
+import com.google.firebase.udacity.mychatapp.databinding.ChatItemviewBinding;
+import com.squareup.picasso.Picasso;
 
 public class ChatActivity extends AppCompatActivity {
+
+    private ActivityChatBinding binding;
+    private FirebaseFirestore firebaseFirestore;
+    private FirebaseAuth firebaseAuth;
+
+    FirestoreRecyclerAdapter<UserModel, ChatHolder> chatAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
+        binding = ActivityChatBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        Query query = firebaseFirestore.collection("users");
+        FirestoreRecyclerOptions<UserModel> options = new FirestoreRecyclerOptions.Builder<UserModel>()
+                .setQuery(query, UserModel.class)
+                .setLifecycleOwner(this)
+                .build();
+
+        chatAdapter = new FirestoreRecyclerAdapter<UserModel, ChatHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ChatHolder holder, int position, @NonNull UserModel model) {
+                holder.binding.chatName.setText(model.getName());
+                String chatImageUri = model.getImage();
+                Picasso.get().load(chatImageUri).into(holder.binding.chatImage);
+                if (model.getStatus().equals("Online")) {
+                    holder.binding.chatStatus.setText(model.getStatus());
+                    holder.binding.chatStatus.setTextColor(Color.BLUE);
+                } else {
+                    // todo: test color on updated
+                    holder.binding.chatStatus.setText(model.getStatus());
+                }
+
+                holder.itemView.setOnClickListener(v -> {
+                    Toast.makeText(getApplicationContext(), "Open chat", Toast.LENGTH_SHORT)
+                            .show();
+                });
+            }
+
+            @NonNull
+            @Override
+            public ChatHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.chat_itemview, parent, false);
+                return new ChatHolder(view);
+            }
+        };
+
+        binding.chatList.setAdapter(chatAdapter);
+
+    }
+
+    public class ChatHolder extends RecyclerView.ViewHolder{
+
+        ChatItemviewBinding binding;
+
+        public ChatHolder(@NonNull View itemView) {
+            super(itemView);
+            binding = ChatItemviewBinding.bind(itemView);
+        }
     }
 }

f853b435f59b6933c85c1a13aa0ff3d332b3704a
Olariaga Martin <olariagamartin@gmail.com>
2021-10-02 01:33:11 -0300

    UpdateProfileViewModel working

--- app/src/main/java/com/themarto/mychatapp/mainActivity/UpdateProfileFragment.java
+++ app/src/main/java/com/themarto/mychatapp/mainActivity/UpdateProfileFragment.java
@@ -3,7 +3,6 @@ package com.themarto.mychatapp.mainActivity;
 import static android.app.Activity.RESULT_OK;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -11,6 +10,7 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
@@ -18,46 +18,27 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 import com.themarto.mychatapp.R;
-import com.themarto.mychatapp.UserModel;
 import com.themarto.mychatapp.databinding.FragmentUpdateProfileBinding;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 
 public class UpdateProfileFragment extends Fragment {
 
     private FragmentUpdateProfileBinding binding;
-    // todo: move all to view model
-    private FirebaseAuth firebaseAuth;
-    private FirebaseFirestore firebaseFirestore;
 
-    private FirebaseStorage firebaseStorage;
-    private StorageReference storageReference;
+    private UpdateProfileViewModel viewModel;
 
-    private UserModel user;
-
-    private Uri imagePath;
-
-    // todo: test
     private ActivityResultLauncher<Intent> pickPhotoLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             result -> {
                 if (result.getResultCode() == RESULT_OK) {
-                    // todo: notify view model
-                    imagePath = result.getData().getData();
-
-                    updateProfileImage();
+                    Uri imagePath = result.getData().getData();
+                    viewModel.onProfileImagePicked(imagePath);
                 }
             }
     );
@@ -65,14 +46,7 @@ public class UpdateProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // todo: move to view model
-        firebaseAuth = FirebaseAuth.getInstance();
-        firebaseFirestore = FirebaseFirestore.getInstance();
-
-        firebaseStorage = FirebaseStorage.getInstance();
-        storageReference = firebaseStorage.getReference();
-
+        viewModel = new ViewModelProvider(this).get(UpdateProfileViewModel.class);
     }
 
     @Override
@@ -80,47 +54,47 @@ public class UpdateProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentUpdateProfileBinding.inflate(inflater, container, false);
 
-        fetchDataFromFirebase();
+        setupClickActions();
 
-        setEditUsernameAction();
+        setupObservers();
 
         return binding.getRoot();
     }
 
-    // todo: move to view model
-    private void fetchDataFromFirebase() {
-        DocumentReference documentReference = firebaseFirestore
-                .collection("users")
-                .document(firebaseAuth.getUid());
-
-        documentReference.get().addOnSuccessListener(documentSnapshot -> {
-            // todo: check for null object
-            user = documentSnapshot.toObject(UserModel.class);
-            loadUserData();
-        }).addOnFailureListener(e -> {
-            Toast.makeText(requireContext(), "Load user data failed", Toast.LENGTH_SHORT)
-                    .show();
-        });
+    private void setupObservers () {
+        viewModel.getUsername().observe(getViewLifecycleOwner(), this::loadUsername);
+        viewModel.getImageLink().observe(getViewLifecycleOwner(), this::loadImage);
+        viewModel.showDialogChangeUsername()
+                .observe(getViewLifecycleOwner(), unused -> showDialogChangeUsername());
+        viewModel.launchImagePicker()
+                .observe(getViewLifecycleOwner(), unused -> launchImagePicker());
+        viewModel.showProgressBar().observe(getViewLifecycleOwner(), this::showProgressBar);
     }
 
-    // todo: observe data from view model
-    private void loadUserData() {
-        Picasso.get().load(user.getImage()).into(binding.profileImage);
-        binding.username.setText(user.getName());
+    private void loadUsername(String username) {
+        binding.username.setText(username);
     }
 
-    // todo: change name
-    // todo: notify view model
-    private void setEditUsernameAction() {
+    private void loadImage (String imageLink) {
+        Picasso.get().load(imageLink).into(binding.profileImage);
+    }
+
+    private void launchImagePicker () {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        pickPhotoLauncher.launch(intent);
+    }
+
+    private void showProgressBar (boolean show) {
+        if (show) binding.progressBar.setVisibility(View.VISIBLE);
+        else binding.progressBar.setVisibility(View.INVISIBLE);
+    }
+
+    private void setupClickActions() {
         binding.editUsername.setOnClickListener(v -> {
-            showDialogChangeUsername();
+            viewModel.onEditUserNameClicked();
         });
         binding.profileImage.setOnClickListener(v -> {
-            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-            // take photo
-            //Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-            pickPhotoLauncher.launch(intent);
+            viewModel.onEditProfileImageClicked();
         });
     }
 
@@ -128,14 +102,14 @@ public class UpdateProfileFragment extends Fragment {
         MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(requireContext());
         View editLayout = getLayoutInflater().inflate(R.layout.dialog_update_username, null);
         EditText username = editLayout.findViewById(R.id.username);
-        username.setText(user.getName());
+        username.setText(viewModel.getUsername().getValue());
         username.requestFocus(); // required for API 28+
         username.setSelection(username.getText().length());
 
         builder.setView(editLayout)
                 .setPositiveButton("Save", (dialog, which) -> {
-                    // todo: send to view model
-                    updateUsername(username.getText().toString());
+                    String nUsername = username.getText().toString();
+                    viewModel.onSaveUsernameClicked(nUsername);
                 })
                 .setNegativeButton("Cancel", null);
         // 1: avoid cut the view when keyboard appears, 2: make the keyboard appear
@@ -143,71 +117,5 @@ public class UpdateProfileFragment extends Fragment {
         dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN
                 | WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
         dialog.show();
-    }
-
-    // todo: move to view model
-    private void updateUsername(String username) {
-        DocumentReference documentReference = firebaseFirestore
-                .collection("users")
-                .document(firebaseAuth.getUid());
-
-        documentReference.update("name", username)
-                .addOnSuccessListener(unused -> {
-                    binding.username.setText(username);
-                });
-    }
-
-    // todo: move to view model
-    private void updateProfileImage() {
-        StorageReference imageRef = storageReference
-                .child("images")
-                .child(firebaseAuth.getUid())
-                .child("profile image");
-
-        byte[] data = getCompressedImage();
-
-        UploadTask uploadTask = imageRef.putBytes(data);
-        binding.progressBar.setVisibility(View.VISIBLE);
-
-        uploadTask.addOnSuccessListener(taskSnapshot -> {
-            imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
-                String imageUriAccessToken = uri.toString();
-                //Toast.makeText(getApplicationContext(), "URI get success", Toast.LENGTH_SHORT).show();
-                updateProfileImageLink(imageUriAccessToken);
-            }).addOnFailureListener(e -> {
-                //Toast.makeText(getApplicationContext(), "URI get failed", Toast.LENGTH_SHORT).show();
-            });
-
-            //Toast.makeText(getApplicationContext(), "Image uploaded", Toast.LENGTH_SHORT).show();
-
-        }).addOnFailureListener(e -> {
-            //Toast.makeText(getApplicationContext(), "Image no uploaded", Toast.LENGTH_SHORT).show();
-        });
-    }
-
-    private byte[] getCompressedImage() {
-        Bitmap bitmap = null;
-        try {
-            bitmap = MediaStore.Images.Media.getBitmap(requireContext().getContentResolver(), imagePath);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 25, byteArrayOutputStream);
-        return byteArrayOutputStream.toByteArray();
-    }
-
-    // todo: move to view model
-    private void updateProfileImageLink(String link) {
-        DocumentReference documentReference = firebaseFirestore
-                .collection("users")
-                .document(firebaseAuth.getUid());
-
-        documentReference.update("image", link)
-                .addOnSuccessListener(unused -> {
-                    binding.profileImage.setImageURI(imagePath);
-                    binding.progressBar.setVisibility(View.INVISIBLE);
-                });
     }
 }
---
+++ app/src/main/java/com/themarto/mychatapp/mainActivity/UpdateProfileViewModel.java
@@ -0,0 +1,133 @@
+package com.themarto.mychatapp.mainActivity;
+
+import static com.themarto.mychatapp.utils.Utils.compressImage;
+
+import android.app.Application;
+import android.net.Uri;
+import android.view.View;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.themarto.mychatapp.UserModel;
+import com.themarto.mychatapp.utils.SingleLiveEvent;
+
+public class UpdateProfileViewModel extends AndroidViewModel {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
+
+    private FirebaseStorage firebaseStorage;
+    private StorageReference storageReference;
+
+    private MutableLiveData<String>  username = new MutableLiveData<>();
+    private MutableLiveData<String>  imageLink = new MutableLiveData<>();
+    private SingleLiveEvent<Void> showDialogChangeUsername = new SingleLiveEvent<>();
+    private SingleLiveEvent<Void> launchImagePicker = new SingleLiveEvent<>();
+    private MutableLiveData<Boolean> showProgressBar = new MutableLiveData<>();
+
+    public UpdateProfileViewModel(Application application) {
+        super(application);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
+
+        fetchUserData();
+    }
+
+    private void fetchUserData () {
+        DocumentReference documentReference = firebaseFirestore
+                .collection("users")
+                .document(firebaseAuth.getUid());
+        documentReference.get().addOnSuccessListener(documentSnapshot -> {
+            // todo: check for null object
+            UserModel model = documentSnapshot.toObject(UserModel.class);
+            username.setValue(model.getName());
+            imageLink.setValue(model.getImage());
+        }).addOnFailureListener(e -> {
+            // notify error
+        });
+    }
+
+    public void onEditUserNameClicked () {
+        showDialogChangeUsername.call();
+    }
+
+    public void onEditProfileImageClicked () {
+        launchImagePicker.call();
+    }
+
+    public void onSaveUsernameClicked (String nUsername) {
+        updateUsername(nUsername);
+    }
+
+    public LiveData<String> getUsername () {
+        return username;
+    }
+
+    public LiveData<String> getImageLink () {
+        return imageLink;
+    }
+    public LiveData<Boolean> showProgressBar () {
+        return showProgressBar;
+    }
+
+    public LiveData<Void> showDialogChangeUsername () {
+        return showDialogChangeUsername;
+    }
+
+    public LiveData<Void> launchImagePicker () {
+        return launchImagePicker;
+    }
+
+    private void updateUsername(String nUsername) {
+        DocumentReference documentReference = firebaseFirestore
+                .collection("users")
+                .document(firebaseAuth.getUid());
+
+        documentReference.update("name", nUsername)
+                .addOnSuccessListener(unused -> {
+                    this.username.setValue(nUsername);
+                });
+    }
+
+    public void onProfileImagePicked (Uri imagePath) {
+        StorageReference imageRef = storageReference
+                .child("images")
+                .child(firebaseAuth.getUid())
+                .child("profile image");
+
+        byte[] data  = compressImage(getApplication(), imagePath);
+        UploadTask uploadTask = imageRef.putBytes(data);
+
+        showProgressBar.setValue(true);
+
+        uploadTask.addOnSuccessListener(taskSnapshot -> {
+            imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                String imageUriAccessToken = uri.toString();
+                updateProfileImageLink(imageUriAccessToken);
+            });
+            showProgressBar.setValue(false);
+        });
+    }
+
+    private void updateProfileImageLink(String link) {
+        DocumentReference documentReference = firebaseFirestore
+                .collection("users")
+                .document(firebaseAuth.getUid());
+
+        documentReference.update("image", link)
+                .addOnSuccessListener(unused -> {
+                    imageLink.setValue(link);
+                });
+    }
+}

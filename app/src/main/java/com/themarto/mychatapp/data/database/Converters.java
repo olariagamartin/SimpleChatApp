93c551976b6dfa73df609541029ad61e91ba2032
Olariaga Martin <olariagamartin@gmail.com>
2021-10-11 16:28:07 -0300

    ChatRepository ready to test

--- app/src/main/java/com/themarto/mychatapp/data/database/Converters.java
+++ app/src/main/java/com/themarto/mychatapp/data/database/Converters.java
@@ -2,9 +2,12 @@ package com.themarto.mychatapp.data.database;
 
 import static com.themarto.mychatapp.utils.Utils.convertToBitmap;
 import static com.themarto.mychatapp.utils.Utils.convertToByteCode;
+import static com.themarto.mychatapp.utils.Utils.getChatRoomId;
 
 import com.themarto.mychatapp.data.domain.ContactModel;
+import com.themarto.mychatapp.data.domain.MessageModel;
 import com.themarto.mychatapp.data.network.ContactDTO;
+import com.themarto.mychatapp.data.network.MessageDTO;
 
 public class Converters {
 
@@ -24,6 +27,37 @@ public class Converters {
         contactEntity.profileImage = imageData;
         contactEntity.online = contactDTO.isOnline();
         return contactEntity;
+    }
+
+    public static MessageModel toMessageModel (MessageEntity messageEntity) {
+        MessageModel messageModel = new MessageModel();
+        messageModel.setMessage(messageEntity.message);
+        messageModel.setReceiverId(messageEntity.receiverId);
+        messageModel.setSenderId(messageEntity.senderId);
+        messageModel.setTimestamp(messageEntity.timestamp);
+        messageModel.setChatRoom(messageEntity.chatRoom);
+        return messageModel;
+    }
+
+    public static MessageEntity toMessageEntity (MessageDTO messageDTO) {
+        MessageEntity messageEntity = new MessageEntity();
+        messageEntity.id = messageDTO.getKey();
+        messageEntity.message = messageDTO.getMessage();
+        messageEntity.receiverId = messageDTO.getReceiverId();
+        messageEntity.senderId = messageDTO.getSenderId();
+        messageEntity.timestamp = messageDTO.getTimestamp();
+        messageEntity.chatRoom = getChatRoomId(messageDTO.getSenderId(), messageDTO.getReceiverId());
+        return messageEntity;
+    }
+
+    public static MessageDTO toMessageDTO (MessageModel messageModel, String key) {
+        MessageDTO messageDTO = new MessageDTO();
+        messageDTO.setKey(key);
+        messageDTO.setMessage(messageModel.getMessage());
+        messageDTO.setReceiverId(messageModel.getReceiverId());
+        messageDTO.setSenderId(messageModel.getSenderId());
+        messageDTO.setTimestamp(messageModel.getTimestamp());
+        return messageDTO;
     }
 
 }
--- app/src/main/java/com/themarto/mychatapp/repository/ChatRepository.java
+++ app/src/main/java/com/themarto/mychatapp/repository/ChatRepository.java
@@ -1,31 +1,81 @@
 package com.themarto.mychatapp.repository;
 
-import androidx.lifecycle.LiveData;
+import static com.themarto.mychatapp.data.database.Converters.toMessageDTO;
+import static com.themarto.mychatapp.data.database.Converters.toMessageEntity;
+import static com.themarto.mychatapp.data.database.Converters.toMessageModel;
 
-import com.google.firebase.database.Query;
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Transformations;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.themarto.mychatapp.data.database.ChatAppDatabase;
+import com.themarto.mychatapp.data.database.MessageDao;
+import com.themarto.mychatapp.data.database.MessageEntity;
 import com.themarto.mychatapp.data.domain.MessageModel;
 import com.themarto.mychatapp.data.network.MessageDTO;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ChatRepository {
 
+    private MessageDao messageDao;
+    private LiveData<List<MessageModel>> messageList;
+    private String chatRoomId;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference chatReference;
+
+    private ChatRepository (Application application, String chatRoomId) {
+        messageDao = ChatAppDatabase.getDatabase(application).messageDao();
+        this.chatRoomId = chatRoomId;
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        chatReference = firebaseDatabase.getReference().child("simpleChatRooms")
+                .child(chatRoomId).child("messages");
+        listenForDBChanges();
+    }
+
+    public static ChatRepository getInstance (Application application, String roomId) {
+        return new ChatRepository(application, roomId);
+    }
+
     public LiveData<List<MessageModel>> getMessages (String roomId) {
-        // TODO
-        return null;
+        return messageList;
     }
 
-    public void updateLocalDB (LiveData<List<MessageDTO>> messageListUpdated) {
-        // TODO
+    public void updateLocalDB (List<MessageDTO> messageListUpdated) {
+        List<MessageEntity> messageEntities = new ArrayList<>();
+        for (MessageDTO messageDTO : messageListUpdated) {
+            messageEntities.add(toMessageEntity(messageDTO));
+        }
+        ChatAppDatabase.databaseWriteExecutor.execute(() -> {
+            messageDao.insertMessages(messageEntities);
+        });
     }
 
-    public Query getMessagesFromNetwork (String roomId) {
-        // TODO
-        return null;
+    public DatabaseReference getMessagesFromNetwork () {
+        return chatReference;
     }
 
-    public Query sendMessage (MessageModel message) {
-        // TODO
-        return null;
+    public Task<Void> sendMessage (MessageModel message) {
+        String messageRef = chatReference.push().getKey();
+        MessageDTO messageToSend = toMessageDTO(message, messageRef);
+        return chatReference.child(messageRef).setValue(messageToSend);
+    }
+
+    private void listenForDBChanges () {
+        messageList = Transformations.map(messageDao.getMessages(chatRoomId), messageEntities -> {
+            List<MessageModel> messageModels = new ArrayList<>();
+            for (MessageEntity messageEntity : messageEntities) {
+                messageModels.add(toMessageModel(messageEntity));
+            }
+            return messageModels;
+        });
     }
 }
